c0b454ef9c70da279105f822c683544b03d3f148
Beatriz Bistaffa Vitor <166538919+bistaffabe@users.noreply.github.com>
2026-07-24 19:22:51 -0300

    Implement syscall pairing logic in student_pair_syscall

--- src/student/pairer.c
+++ src/student/pairer.c
@@ -1,32 +1,41 @@
 #include "student_api.h"
+#include "trace_helpers.h"
+#include <string.h>
+#include <sys/syscall.h>
+
+char saved_execve_path[256];  
+
 
 int student_pair_syscall(struct syscall_pairer *pairer,
                          const struct syscall_event *ev,
                          struct syscall_event *out)
 {
-    /*
-     * TODO Semana 2:
-     *
-     * O runtime chama esta funcao duas vezes para cada syscall:
-     *
-     *   1. uma vez antes da syscall executar
-     *   2. uma vez depois da syscall terminar
-     *
-     * Na primeira parada, os argumentos estao disponiveis.
-     * Na segunda parada, o retorno esta disponivel.
-     *
-     * Seu trabalho e produzir um evento completo apenas quando ja existirem
-     * as duas metades da syscall.
-     *
-     * Dicas:
-     * - ev->entering == 1 indica entrada de syscall.
-     * - ev->entering == 0 indica saida de syscall.
-     * - para comecar, assuma apenas um processo monitorado.
-     *
-     * Retorne:
-     *   1 se out contem uma syscall completa
-     *   0 se ainda nao ha syscall completa
-     *  -1 se a sequencia de eventos parece invalida
-     */
-    return 0;
+
+    if (ev->entering == 1) {  
+        pairer->entry = *ev;
+        pairer->has_entry = 1;
+
+       
+        if (ev->syscall_no == SYS_execve) {
+            if (read_child_string(ev->pid, ev->args[0],
+                                  saved_execve_path,
+                                  sizeof(saved_execve_path)) < 0)
+                snprintf(saved_execve_path, sizeof(saved_execve_path), "<ilegivel>");
+        }
+
+        return 0;
+    }
+
+    if (!pairer->has_entry) {
+        return -1;
+    }
+    *out = pairer->entry;
+    out->ret = ev->ret;
+    out->entering = 0;
+
+    pairer->has_entry = 0;
+
+    return 1;
+    
 }
+
